6c552d04dc41494b5be41c61cfde925039b6a784
mithun2k2 <mithun2k2eee@gmail.com>
2024-12-19 19:21:19 +0000

    Balance index finder program added

---
+++ src/com/qaScript/BalanceIndexFinder.java
@@ -0,0 +1,54 @@
+package com.qaScript;
+
+/*
+ * You are going to be given an array of integers.
+ * Your job is to take that array and find an index N where the
+ * sum of the integers to the left of N is equal to the sum of the
+ * integers to the right of N.
+
+If there is no index that would make this happen, return -1.
+
+For example:
+Let's say you are given the array {1,2,3,4,3,2,1}:
+Your function will return the index 3, because at the 3rd position of the array, the sum of left side of the index ({1,2,3}) and the sum of the right side of the index ({3,2,1}) both equal 6.
+
+Let's look at another one.
+You are given the array {1,100,50,-51,1,1}:
+Your function will return the index 1, because at the 1st position of the array, the sum of left side of the index ({1}) and the sum of the right side of the index ({50,-51,1,1}) both equal 1.
+
+Last one:
+You are given the array {20,10,-80,10,10,15,35}
+At index 0 the left side is {}
+The right side is {10,-80,10,10,15,35}
+They both are equal to 0 when added. (Empty arrays are equal to 0 in this problem)
+Index 0 is the place where the left side and right side are equal.
+
+Note: Please remember that in most languages the index of an array starts at 0.
+
+ */
+public class BalanceIndexFinder {
+    public static int findBalanceIndex(int[]arr){
+        int totalSum =0;
+        for(int num: arr){
+            totalSum+=num;
+        }
+        //Initialize the left sum zero
+        int leftSum =0;
+        // Iterate through the array to find the balance index
+        for(int i=0; i<arr.length; i++){
+            int rightSum = totalSum - leftSum - arr[i]; // Calculate right sum
+            // Check if the leftsum equals the right sum
+            if(leftSum==rightSum){
+                return i; // Return the balance index
+            }
+            leftSum+= arr[i];// Update for next iteration
+        }
+        return -1;
+    }
+
+    public static void main(String[]args){
+        int[] arr = {1,100,50,-51,1,1};
+        int index = findBalanceIndex(arr);
+        System.out.println(index);
+    }
+}
---
+++ src/com/qaScript/HumanReadableTime.java
@@ -0,0 +1,62 @@
+package com.qaScript;
+
+/*
+ * Write a function, which takes a non-negative
+ * integer (seconds) as input and returns the time in a human-readable
+ * format (HH:MM:SS)
+ *HH = hours, padded to 2 digits, range: 00 - 99
+ *MM = minutes, padded to 2 digits, range: 00 - 59
+ *SS = seconds, padded to 2 digits, range: 00 - 59
+ *The maximum time never exceeds 359999 (99:59:59)
+* */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HumanReadableTime {
+    public static String makeReadable(int seconds){
+        if(seconds==0){
+            return "now";
+        }
+        List<String> timeParts = new ArrayList<>();
+        int years = seconds/31536000; // 60*60*24*365
+        if(years>0){
+            timeParts.add(years + " year" + (years>1? "s": "")); // Pluralization
+            seconds%=31536000;
+        }
+
+        int days = seconds/86400; // 60*60*24
+        if(days>0){
+            timeParts.add(days+ " day" + (days>1? "s": ""));// Pluralization
+            seconds%= 86400;
+        }
+
+        int hours = seconds/3600; // 60*60
+        if(hours>0){
+            timeParts.add(hours + " hour" + (hours>1? "s": ""));// Pluralization
+            seconds%= 3600;
+        }
+
+        int minutes = seconds/60; // 60*60
+        if(minutes>0){
+            timeParts.add(minutes + " minute"+ (minutes>1? "s": ""));// Pluralization
+            seconds%= 60;
+        }
+
+        if(seconds > 0) {
+            timeParts.add(seconds + " second"+ (seconds>1? "s": ""));
+        }
+        if(timeParts.size()>1){
+            String lastPart = timeParts.remove(timeParts.size()-1);
+            return String.join(", ", timeParts) + " and " + lastPart;
+        }
+
+        return Integer.toString(seconds);
+    }
+
+    public static void main(String[]args){
+        int inputSeconds = 85000;
+        String result = makeReadable(inputSeconds);
+        System.out.println("Formatted duration " + result);
+    }
+}
