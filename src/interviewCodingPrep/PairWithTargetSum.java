df5ac8d5f2a4ffd097f421d793e4536d9e406292
mithun2k2 <mithun2k2eee@gmail.com>
2024-12-26 17:40:28 +0000

    added

--- src/interviewCodingPrep/PairWithTargetSum.java
+++ src/interviewCodingPrep/PairWithTargetSum.java
@@ -3,7 +3,9 @@ package interviewCodingPrep;
 import java.util.HashMap;
 
 /*
-Given an array arr[] of positive integers and another integer target. Determine if there exists two distinct indices such that the sum of there elements is equals to target.
+Given an array arr[] of positive integers and another integer target.
+Determine if there exists two distinct indices such that the sum of
+their elements is equals to target.
 
 Examples:
 
